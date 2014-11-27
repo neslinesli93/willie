57e576c7fae1fd05423e51f6bb5a226b34e4e84c
Tom Pom <p.tommy93@gmail.com>
2014-11-27 14:10:55 +0100

    Localizzato help.py

--- willie/modules/help.py
+++ willie/modules/help.py
@@ -30,7 +30,7 @@ def setup(bot=None):
 def help(bot, trigger):
     """Shows a command's documentation, and possibly an example."""
     if not trigger.group(2):
-        bot.reply('Say .help <command> (for example .help c) to get help for a command, or .commands for a list of commands.')
+        bot.reply('Scrivi .help <comando> (ad esempio .help c) per avere info sul comando, o .commands per una lista di comandi.')
     else:
         name = trigger.group(2)
         name = name.lower()
@@ -43,7 +43,7 @@ def help(bot, trigger):
         if name in bot.doc:
             if len(bot.doc[name][0]) + (1 if bot.doc[name][1] else 0) > threshold:
                 if trigger.nick != trigger.sender:  # don't say that if asked in private
-                    bot.reply('The documentation for this command is too long; I\'m sending it to you in a private message.')
+                    bot.reply('L\'help di questo comando è troppo lungo, te lo mando in query.')
                 msgfun = lambda l: bot.msg(trigger.nick, l)
             else:
                 msgfun = bot.reply
@@ -60,18 +60,17 @@ def commands(bot, trigger):
     """Return a list of bot's commands"""
     names = ', '.join(sorted(iterkeys(bot.doc)))
     if not trigger.is_privmsg:
-        bot.reply("I am sending you a private message of all my commands!")
-    bot.msg(trigger.nick, 'Commands I recognise: ' + names + '.', max_messages=10)
-    bot.msg(trigger.nick, ("For help, do '%s: help example' where example is the " +
-                           "name of the command you want help for.") % bot.nick)
+        bot.reply("Ti sto mandando tutti i comandi in query")
+    bot.msg(trigger.nick, 'Comandi: ' + names + '.', max_messages=10)
+    bot.msg(trigger.nick, ("Per aiuto sui singoli comandi, fai '%s: help comando'") % bot.nick)
 
 
 @rule('$nick' r'(?i)help(?:[?!]+)?$')
 @priority('low')
 def help2(bot, trigger):
     response = (
-        'Hi, I\'m a bot. Say ".commands" to me in private for a list ' +
-        'of my commands, or see http://willie.dftba.net for more ' +
-        'general details. My owner is %s.'
+        'Ciao, io sono un bot. Scrivimi ".commands" in query per una lista ' +
+        'dei comandi supportati, o visita http://willie.dftba.net per i ' +
+        'dettagli tecnici. Il mio padrone è %s.'
     ) % bot.config.owner
     bot.reply(response)
